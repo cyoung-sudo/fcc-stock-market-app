53fcada3f53e7618b6eee23ba61061da9a3d1ad1
Christopher Young <christopheryoung@MacBook123.local>
2022-09-01 16:21:24 -0700

    project setup

---
+++ server.js
@@ -0,0 +1,18 @@
+//----- Imports
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const db = require("./db/conn");
+
+//----- Middleware
+app.use(cors());
+app.use(express.json());
+ 
+//----- Connection
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  // Perform DB connection when server starts
+  db.connect();
+  console.log(`Server is running on port: ${port}`);
+});
